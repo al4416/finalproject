8143ddd45c158249a44b4e039cea2394621a53e0
Alex Lee <al4416@nyu.edu>
2016-12-12 04:32:20 +0000

    components input and output

---
+++ app/components/calculator-input.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+
+export default Ember.Component.extend({
+      model() {
+    return {};
+  },
+  actions: {
+    save() {
+    this.get(calculator-output);
+    },
+    reset() {
+
+    }
+  }
+});
